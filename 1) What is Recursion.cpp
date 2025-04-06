c57a8cfb48c93fe0db5562c630663e24df9006b2
Naman goyal <naman.eclipse@gmail.com>
2025-04-07 00:23:36 +0530

    Create 1) What is Recursion.cpp

---
+++ 1) What is Recursion.cpp
@@ -0,0 +1,5 @@
+Recursion is a process of calling a function again and again if we know that our subproblem is exactlyy similar to the main problem.
+
+  Recursion is used at a lot of places and in different , we will write all type of questions in which recursion can be used.
+
+Recursion is taking decisions rather than making the input small , we work on taking a particular decision and inputs get smaller and smaller by every turn .
